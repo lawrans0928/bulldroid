d6c2238ffef83d7abcbced946ca4d73fc2ff50a5
Lawrans <lawrans96@gmail.com>
2026-03-17 22:47:36 +0530

    changers updated

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -4,10 +4,11 @@ import "../../tailwind.css";
 import Home  from "../../pages/Home";
 import About from "../../pages/About";
 import  Contact  from "../../pages/Contact";
-import Services from "../../pages/Services";
-import Blogs from "../../pages/Blogs";
+import {Services} from "../../pages/Services";
+// import Blogs from "../../pages/Blogs";
 // import Dashboard from "../../pages/dashboard";
 import Dashboard from "../../pages/dashboard";
+import Application from "../../pages/Application";
 
 
 export default function App() {
@@ -16,11 +17,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Services />} />
-        <Route path="/blogs" element={<Blogs />} />
+        {/* <Route path="/products" element={<Services />} /> */}
+        {/* <Route path="/blogs" element={<Blogs />} /> */}
         <Route path="/contact" element={<Contact />} />
         <Route path="/application" element={<Application />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
   );
